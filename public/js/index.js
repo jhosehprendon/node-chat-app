4dabfcf5bfee7348efa11ab4a5b2570119dc4bea
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-29 09:12:16 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function() {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-        to: 'Jen',
-        text:'Hey lady'
-    });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Mike',
-        text: 'Hey fucker!',
-        ceatedAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createEmail', {message});
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
